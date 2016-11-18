aedb05d5ee5fd7941ee8f03bb5eeb3c13521fd5a
Michael <Michael@RATHZ15NB01>
2016-11-18 12:25:21 +0100

    Einlesen der Fragen

--- FahrschulSimulator/src/bl/Frage.java
+++ FahrschulSimulator/src/bl/Frage.java
@@ -2,6 +2,7 @@
 package bl;
 
 import java.awt.Image;
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -11,8 +12,20 @@ public class Frage implements Serializable
     private int nummer;
     private ArrayList<Antwort> antworten;
     private Image bild;
+    private String pfadBild = System.getProperty("user.dir")+ File.separator + "src" + File.separator + "imgs"+File.separator;
     
     private static int anzFragen = 0;
+
+    public Frage(String frage, int nummer, ArrayList<Antwort> antworten, String pfadBild) 
+    {
+        this.frage = frage;
+        this.nummer = nummer;
+        this.antworten = antworten;
+        
+        this.pfadBild += pfadBild;
+    }
+    
+    
 
     public String getFrage()
     {
@@ -37,6 +50,11 @@ public class Frage implements Serializable
     public static int getAnzFragen()
     {
         return anzFragen;
+    }
+
+    public void setAntworten(ArrayList<Antwort> antworten) 
+    {
+        this.antworten = antworten;
     }
     
     
